e0a17f405a35addec406e1da5bc41b15682d8258
mud1tx <muditagarwalna@gmail.com>
2023-12-19 23:52:38 +0530

    invite code logic completed

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,4 +1,5 @@
 import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
@@ -19,7 +20,41 @@ const InviteCodePage = async ({ params }: InviteCodePageProps) => {
     return redirect("/");
   }
 
-  return <div>InviteCodePage</div>;
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          },
+        ],
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return null;
 };
 
 export default InviteCodePage;
