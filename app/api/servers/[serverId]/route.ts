17a73fd1b5bdc3c61e262519dad2d066d34e513f
mud1tx <muditagarwalna@gmail.com>
2023-12-20 23:18:50 +0530

    added delete and leave server logic

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
 import { NextResponse } from "next/server";
 
 import { currentProfile } from "@/lib/current-profile";
@@ -34,6 +33,35 @@ export async function PATCH(
     return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVERS_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse("Server ID Missing", { status: 400 });
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVERS_ID_DELETE]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
